3553570fbe259f23c87e4bb719a40ec8db93c900
Alexandr Makarov <alexandr.makarov.am@icloud.com>
2024-03-21 22:08:34 +0300

    set changes

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const http = require('http');
+const axios = require('axios');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+let queue = [];
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/queue/list', (req, res) => {
+    res.status(200).json(JSON.stringify(queue));
+})
+
+app.get('/queue/clear', (req, res) => {
+    queue = [];
+    res.status(200).json(JSON.stringify(queue));
+})
+
+app.post('/queue/add', (req, res) => {
+    if (Array.isArray(req.body)) {
+        queue = [...queue, ...req.body];
+        return res.status(200).json("ok");
+    }
+    res.status(400).json("wrong payload");
+})
+
+http.createServer(app).listen(process.env.PORT || 3000, undefined, undefined, () => {
+    let i = 0;
+    setInterval(() => {
+        const index = ++i % queue.length;
+        const item = queue[index];
+        if (item) {
+            const { url, method, data, hook } = item;
+            axios.request({
+                method,
+                url,
+                data
+            }).then((res) => {
+                axios.post(hook, { content: JSON.stringify(res.data) })
+            })
+        }
+    }, 2000);
+});
